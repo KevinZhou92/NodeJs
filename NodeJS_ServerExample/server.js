67fbc498f707a973f771e1d5f0e800045ffecee6
KevinZh92 <monsterzpc@gmail.com>
2017-11-04 17:30:26 -0400

    node server example

--- NodeJS_ServerExample/server.js
+++ NodeJS_ServerExample/server.js
@@ -1,23 +1,58 @@
 const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
 
-
+// this will return an instance of a web server
 var app = express();
 
+// set the template engine as hbs
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+app.use((req, res, next) => {
+    var time = new Date();
+    var logMessage = `${time}: ${req.method} ${req.url} `;
+    console.log(logMessage);
+    fs.appendFile('server.log', logMessage + '\n', (error) => {
+        if (error) {
+            console.log('Unable to append log file!');
+        }
+    });
+    next();
+});
+// server the static page
+app.use(express.static(__dirname + '/public'));
+
+// maintenance use
+// app.use((req, res, next) => {
+//     res.render('maintenance')
+// });
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
-app.get('/about', (req, res) => {
-    res.send({
-        name: 'Kevin',
-        likes: [
-            'Guitar',
-            'Rock'
-        ]
+    res.render('home', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!',
     });
 });
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        pageTitle: 'About Page',
+        welcomeMessage: 'About my website.'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        Error: 'Unable to fetch data!'
+    });
+});
+// activate the app to listen on port 8080 of local machine
 app.listen(8080, () => {
     console.log('Starting listening on 8080...');
 });
